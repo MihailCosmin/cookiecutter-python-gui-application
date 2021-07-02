09b0ee2235025af437c3b523ce2034a90f84dc32
Mihail-Cosmin Munteanu <munteanu@althom.de>
2021-07-02 13:12:50 +0200

    Added PyQt5 example in main module

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,1 +1,24 @@
 """Main module."""
+
+{% if cookiecutter.gui_framework == 'PyQt5' -%}
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.setWindowTitle('{{ cookiecutter.project_name }}')
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+{% endif %}
