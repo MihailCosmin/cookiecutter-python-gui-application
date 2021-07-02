12e4c00c21465e2af9c2822ea3a3ec1f9f2535ea
Mihail-Cosmin Munteanu <munteanu@althom.de>
2021-07-02 12:47:52 +0200

    Initial Commit
    Initial Commit to test the functionality

---
+++ README.rst
@@ -0,0 +1,53 @@
+===================================
+cookiecutter-python-gui-application
+===================================
+
+Cookiecutter template for a Python GUI Application based on cookiecutter-pypackage: https://github.com/audreyr/cookiecutter.
+
+* Free software: BSD license
+* Pytest_ runner: Supports `unittest`, `pytest`, `nose` style tests and more
+* Travis-CI_: Ready for Travis Continuous integration testing
+* Tox_ testing: Setup to easily test for python 2.6, 2.7, 3.3 and PyPy_
+* Sphinx_ docs: Documentation raedy for generation with, for example, ReadTheDocs_
+* Wheel_ support: Use the newest python package distribution standard from the get go
+
+Usage
+-----
+
+Generate a Python package project::
+
+    cookiecutter https://github.com/MigailCosmin/cookiecutter-python-gui-application.git
+
+Then:
+
+* Create a repo and put it there.
+* Add the repo to your Travis CI account.
+* Add the repo to your ReadTheDocs account + turn on the ReadTheDocs service hook.
+* Run `tox` to make sure all tests pass.
+* Release your package the standard Python way.
+
+Not Exactly What You Want?
+--------------------------
+
+Don't worry, you have options:
+
+Similar Cookiecutter Templates
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+* `audreyr/cookiecutter-pypackage`_: The original pypackage.
+
+Fork This
+~~~~~~~~~
+
+If you have differences in your preferred setup, I encourage you to fork this
+to create your own version. Once you have your fork working, add it to the
+Similar Cookiecutter Templates list with a brief explanation. It's up to you
+whether or not to rename your fork.
+
+Or Submit a Pull Request
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+I also accept pull requests on this, if they're small, atomic, and if they
+make my own packaging experience better.
+
+
---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+{%- if cookiecutter.gui_framework == 'PyQT5' %}'PyQt5>=5.15.4',
+{% elif cookiecutter.gui_framework == 'PyQt6' %}'PyQt6>=6.1.1',
+{% elif cookiecutter.gui_framework == 'PySide2' %}'PySide2>=5.15.2',
+{% elif cookiecutter.gui_framework == 'PySide6' %}'PySide6>=6.1.2',
+{% elif cookiecutter.gui_framework == 'Kivy' %}'Kivy>=2.0.0',
+{% elif cookiecutter.gui_framework == 'wxPython' %}'wxPython>=4.1.1',
+{% elif cookiecutter.gui_framework == 'Pyforms' %}'Pyforms>=4.0.3',
+{% elif cookiecutter.gui_framework == 'PySimpleGUI' %}'PySimpleGUI>=4.45.0',
+{% elif cookiecutter.gui_framework == 'PyGTK' %}'PyGTK>=2.24.2',
+{%- endif %}
+]
+
+test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest>=3',{%- endif %} ]
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
+    author_email='{{ cookiecutter.email }}',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+{%- if cookiecutter.open_source_license in license_classifiers %}
+        '{{ license_classifiers[cookiecutter.open_source_license] }}',
+{%- endif %}
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="{{ cookiecutter.project_short_description }}",
+    install_requires=requirements,
+{%- if cookiecutter.open_source_license in license_classifiers %}
+    license="{{ cookiecutter.open_source_license }}",
+{%- endif %}
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='{{ cookiecutter.project_slug }}',
+    name='{{ cookiecutter.project_slug }}',
+    packages=find_packages(include=['{{ cookiecutter.project_slug }}', '{{ cookiecutter.project_slug }}.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    zip_safe=False,
+)
